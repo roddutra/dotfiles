3730fb5d171b6bc7cb95521683ee142ce32ddc8d
Rod Dutra <rodrigofd87@gmail.com>
2025-09-05 16:32:35 +1000

    Add tmux integration keymaps to Neovim
    - Add direct tmux control keymaps using <leader>t prefix
    - Configure which-key menu to show Tmux submenu with icon
    - Document new keymaps in README
    - Solves issue where tmux prefix key (Ctrl+Space) doesn't work from within nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Tmux integration keymaps (using <leader>t prefix to avoid conflicts)
+vim.keymap.set("n", "<leader>tw", "<cmd>silent !tmux choose-window<CR>", { desc = "Tmux: Choose window" })
+vim.keymap.set("n", "<leader>ts", "<cmd>silent !tmux choose-session<CR>", { desc = "Tmux: Choose session" })
+vim.keymap.set("n", "<leader>tc", "<cmd>silent !tmux new-window<CR>", { desc = "Tmux: New window" })
+vim.keymap.set("n", "<leader>tx", "<cmd>silent !tmux kill-window<CR>", { desc = "Tmux: Kill current window" })
+vim.keymap.set("n", "<leader>tn", "<cmd>silent !tmux next-window<CR>", { desc = "Tmux: Next window" })
+vim.keymap.set("n", "<leader>tp", "<cmd>silent !tmux previous-window<CR>", { desc = "Tmux: Previous window" })
+vim.keymap.set("n", "<leader>tr", "<cmd>!tmux command-prompt 'rename-window %%'<CR>", { desc = "Tmux: Rename window" })
+vim.keymap.set("n", "<leader>td", "<cmd>silent !tmux detach-client<CR>", { desc = "Tmux: Detach session" })
---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      { "<leader>t", group = "Tmux", icon = "󰖲" },
+    },
+  },
+}
